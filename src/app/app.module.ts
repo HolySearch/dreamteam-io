643528a7b94adc44c0bd0e401c8fc03473a82d6f
Jonas <jonas.custodio00@gmail.com>
2018-08-09 20:29:03 -0300

    Adição de Banner, normalização de blocos de lista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { DefaultBannerComponent } from './default-banner/default-banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutUsComponent,
-    MenuBarComponent
+    MenuBarComponent,
+    DefaultBannerComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/default-banner/default-banner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-default-banner',
+  templateUrl: './default-banner.component.html',
+  styleUrls: ['./default-banner.component.css']
+})
+export class DefaultBannerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
